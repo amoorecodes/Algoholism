22f2d22d05254eeaaa28f82a2fff72c5deaf937c
Arthur Ryabtsev <ryabtsev.art@gmail.com>
2020-07-16 11:38:33 -0700

    [add] power solution

---
+++ Recursion_Problems/easy_recursion.js
@@ -0,0 +1,28 @@
+function power(base, exp) {
+  // [2, 4] => 2*2 = 4 => 4 * 2 => 8
+
+  let result = 1;
+
+  if (exp === 0) return 1;
+  if (exp === 1) return base;
+
+  while (exp > 0) {
+    result = multiply(result, base);
+    exp--;
+  }
+
+  function multiply(current, number) {
+    return current * number;
+  }
+
+  return result;
+}
+
+// console.log(power(2, 0));
+// console.log(power(2, 1));
+// console.log(power(2, 2));
+// console.log(power(2, 3));
+// console.log(power(2, 4));
+// console.log(power(2, 5));
+// console.log(power(2, 6));
+// console.log(power(2, 7));
